fc59714b187be0d8a32fd31d7deba387df5b798b
Alan <alantorresm86@gmail.com>
2022-03-18 11:41:31 -0700

    Update vcs.xml, MainActivity.kt, and 6 more files...

---
+++ app/src/main/java/mx/kodemia/personalweather/model/weather/Current.kt
@@ -0,0 +1,15 @@
+package mx.kodemia.personalweather.model.weather
+
+import mx.kodemia.personalweather.model.weather.Weather
+
+data class Current(
+    val dt: Long,
+    val sunrise: Long,
+    val sunset: Long,
+    val temp: Double,
+    val feels_like: Double,
+    val pressure: Int,
+    val humidity: Int,
+    val wind_speed: Double,
+    val weather: List<Weather>
+)
