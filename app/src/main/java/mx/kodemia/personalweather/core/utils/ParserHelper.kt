6a016d81987482545c1d10fc200db4f9f25b7a7b
Alan Torres <alantorresm86@gmail.com>
2022-03-30 14:51:36 -0700

    added spanish translation and fix day locale

--- app/src/main/java/mx/kodemia/personalweather/core/utils/ParserHelper.kt
+++ app/src/main/java/mx/kodemia/personalweather/core/utils/ParserHelper.kt
@@ -3,7 +3,7 @@ package mx.kodemia.personalweather.core.utils
 import java.text.SimpleDateFormat
 import java.util.*
 
-fun hourParser(time: Long): String = SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(time * 1000))
+fun hourParser(time: Long): String = SimpleDateFormat("hh:mm a", Locale.getDefault()).format(Date(time * 1000))
 
 fun capitalizeText(text: String): String {
     return if (text.isNotEmpty())
@@ -11,4 +11,4 @@ fun capitalizeText(text: String): String {
     else ""
 }
 
-fun dayParser(day: Long): String = SimpleDateFormat("EEEE", Locale.ENGLISH).format(Date(day * 1000))
+fun dayParser(day: Long): String = SimpleDateFormat("EEEE", Locale.getDefault()).format(Date(day * 1000)).uppercase()
